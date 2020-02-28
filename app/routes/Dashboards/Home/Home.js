ba50871ea170eee94cbc7c2916330b1babcdcd5e
Linternator <up607958@myport.ac.uk>
2020-02-28 12:34:02 +0000

    Added quick status test.

--- app/routes/Dashboards/Home/Home.js
+++ app/routes/Dashboards/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import {
     Container,
@@ -16,19 +16,77 @@ import {
 import { setupPage } from './../../../components/Layout/setupPage';
 import { HeaderMain } from "../../components/HeaderMain";
 
-const Home = () => (
-    <Container>
-        <Row className="mb-2">
-            <Col lg={ 12 }>
-                <HeaderMain 
-                    title="Home"
-                    className="mb-4 mb-lg-5"
-                />
+const GetStatus = function (component) {
+    fetch('https://localhost:5003/status/system')
+    .then(results => {
+        return results.json();
+    }).then(data => {
+        if(!data.isOk){
+            if(data.message.startsWith('[WARN]')){
+                component.setState({ systemStatus: {error_type: "Warn", error_message: data.message.replace('[WARN] ',''), badgeColor: "warning"}})
+            } else if(data.message.startsWith('[ERROR]')){
+                component.setState({ systemStatus: {error_type: "Error", error_message: data.message.replace('[ERROR] ',''), badgeColor: "danger"}})
+            } else {
+                component.setState({ systemStatus: {error_type: "Unknown", error_message: "", badgeColor: "warning"}})
+            }
+        } else {
+            component.setState({ systemStatus: {error_type: "Ok", error_message: "", badgeColor: "success"}})
+        }
+    }).catch(error => {
+        component.setState({ systemStatus: {error_type: "Unknown", error_message: "", badgeColor: "warning"}})
+    });
+}
 
-            </Col>
-        </Row>
-    </Container>
-);
+class Home extends Component {
+
+    state = {
+        systemStatus: {}
+    }
+
+    componentWillMount() {
+
+        GetStatus(this);
+        this.interval = setInterval(() => GetStatus(this), 10000);
+
+    }
+
+    componentWillUnmount() {
+      clearInterval(this.interval);
+    }
+
+    render() {
+
+        return (
+            <Container>
+                <Row className="mb-2">
+                    <Col lg={ 12 }>
+                        <HeaderMain 
+                            title="Home"
+                            className="mb-4 mb-lg-5"
+                        />
+                    </Col>
+                </Row>
+                <Row>
+                    <Card className="mb-3">
+                        <CardBody>
+                            <CardTitle tag="h6">
+                                System Status <Badge color={this.state.systemStatus.badgeColor}>{this.state.systemStatus.error_type}</Badge>
+                            </CardTitle>
+                            <p className="card-text">
+                                 {/* <Badge color="success">Ok</Badge> Kube Status <br/>
+                                 <Badge color="success">Ok</Badge> Cluster Status<br/>
+                                 <Badge color="warning">Warning</Badge> Ingress Status<br/>
+                                 <Badge color="success">Ok</Badge> Controller Status<br/> */}
+                            </p>                            
+                        </CardBody>
+                    </Card>
+                </Row>
+            </Container>
+        )
+
+    }
+
+}
 
 export default setupPage({
     pageTitle: 'Home'
